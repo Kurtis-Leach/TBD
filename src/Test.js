53367be6345bfe31a42c1127417eefdd9ef16a6e
BobNarwhalKing <danceflurry4366@gmail.com>
2019-07-09 15:37:16 -0500

    Frontend Button Functionality

---
+++ src/Test.js
@@ -0,0 +1,60 @@
+// new Deck()
+
+const newDeck = 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1'
+// let getCard = `https://deckofcardsapi.com/api/deck/${fetchDeck(newDeck)}/draw/?count=`
+
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    let startBtn = document.getElementById('start')
+    // let gameBtnDiv = document.getElementsByClassName('game-btns')
+    let hitBtn = document.getElementById('hit')
+    let stayBtn = document.getElementById('stay')
+    let total = document.getElementById('total')
+    hitBtn.style.display = 'none'
+    stayBtn.style.display = 'none'
+    total.style.display = 'none'
+
+    startBtn.addEventListener('click', ()=>{
+        hitBtn.style.display = 'inline'
+        stayBtn.style.display = 'inline'
+        // total.style.display = 'inline'
+        getDeck(newDeck).then((deck)=> {
+            UserHand.currentHand = []
+           new UserHand(deck.deck_id)
+            
+        })
+
+    })
+
+    hitBtn.addEventListener('click', ()=>{
+        UserHand.currentHand[0].hit()
+    })
+
+    stayBtn.addEventListener('click', ()=>{
+        UserHand.currentHand[0].stay()
+        hitBtn.style.display = 'none'
+        stayBtn.style.display = 'none'
+    })
+
+
+    
+})
+
+
+
+function getDeck(deckUrl){
+let fet = fetch(deckUrl)
+    .then(rsp => rsp.json())
+   return fet
+}  
+        
+
+
+function dealCards(url, num){
+    let address = url + num
+    fetch(address)
+    .then(rsp => rsp.json())
+    .then(result => {
+        console.log(result)
+    })
+}
